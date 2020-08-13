cc148ac488f99669b19052a2d3f2f31ef4787c2e
Artur Drummond <arturbdr@gmail.com>
2020-08-13 15:54:38 +0200

    added log

--- src/main/java/com/poc/ibmmq/gateway/queue/ibmmq/IBMMqGatewayImpl.java
+++ src/main/java/com/poc/ibmmq/gateway/queue/ibmmq/IBMMqGatewayImpl.java
@@ -2,18 +2,25 @@ package com.poc.ibmmq.gateway.queue.ibmmq;
 
 import com.poc.ibmmq.properties.AppProperties;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class IBMMqGatewayImpl {
 
     private final JmsTemplate jmsTemplate;
     private final AppProperties appProperties;
 
     public void sendMessage(final String message) {
-        jmsTemplate.convertAndSend(appProperties.getQueuename(), message.getBytes());
+        try {
+            jmsTemplate.convertAndSend(appProperties.getQueuename(), message.getBytes());
+        } catch (final Exception e) {
+            log.error("Failed to send message", e);
+        }
+
     }
 
 
