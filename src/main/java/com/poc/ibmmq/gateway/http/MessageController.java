137da42fddcd0fd5ad9ceff3cc8207c46d77bc77
Artur Drummond <arturbdr@gmail.com>
2020-08-17 16:56:14 +0200

    updated spring boot version updated the accepted/expected content type

--- src/main/java/com/poc/ibmmq/gateway/http/MessageController.java
+++ src/main/java/com/poc/ibmmq/gateway/http/MessageController.java
@@ -3,6 +3,7 @@ package com.poc.ibmmq.gateway.http;
 import com.poc.ibmmq.usecase.SendNewMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,7 @@ public class MessageController {
 
     private final SendNewMessage sendNewMessage;
 
-    @PostMapping(value = "message")
+    @PostMapping(value = "message", consumes = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> createNewMessage(@RequestBody String newMessage) {
         log.info("Received message {}", newMessage);
         sendNewMessage.sendMessage(newMessage);
