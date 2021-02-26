52e121f65d0434aab8fd0fbbf514aa710766179a
Artur Drummond <arturbdr@gmail.com>
2021-02-26 22:19:55 +0100

    feat:
    updated mq driver version
    added a minor documentation for the endpoint

--- src/main/java/com/poc/ibmmq/gateway/http/MessageController.java
+++ src/main/java/com/poc/ibmmq/gateway/http/MessageController.java
@@ -1,13 +1,16 @@
 package com.poc.ibmmq.gateway.http;
 
 import com.poc.ibmmq.usecase.SendNewMessage;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+import static org.springframework.http.MediaType.TEXT_XML_VALUE;
 
 @RestController
 @Slf4j
@@ -16,16 +19,10 @@ public class MessageController {
 
     private final SendNewMessage sendNewMessage;
 
-    @PostMapping(value = "message", consumes = MediaType.TEXT_PLAIN_VALUE)
+    @ApiOperation(value = "create a new message for IBMMq", consumes = TEXT_PLAIN_VALUE)
+    @PostMapping(value = "message", consumes = {TEXT_PLAIN_VALUE, TEXT_XML_VALUE})
     public ResponseEntity<String> createNewMessage(@RequestBody String newMessage) {
         log.info("Received message {}", newMessage);
-        sendNewMessage.sendMessage(newMessage);
-        return ResponseEntity.ok(newMessage);
-    }
-
-    @PostMapping(value = "orderplacement", consumes = MediaType.TEXT_XML_VALUE)
-    public ResponseEntity<String> createNewOrderPlace(@RequestBody String newMessage) {
-        log.info("Received message createNewOrderPlace {}", newMessage);
         sendNewMessage.sendMessage(newMessage);
         return ResponseEntity.ok(newMessage);
     }
